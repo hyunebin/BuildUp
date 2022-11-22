2153724d23ff9ba1d0c9530e4ecf781e21875f7a
CHANHO PARK <56587688+chanho-datcom@users.noreply.github.com>
2022-11-22 23:59:37 +0900

    test : test (#2)
    Body(Description) : 설명
    
    Footer : 꼬리말

--- src/main/java/com/buildup/bu/BuApplication.java
+++ src/main/java/com/buildup/bu/BuApplication.java
@@ -2,8 +2,10 @@ package com.buildup.bu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BuApplication {
 
     public static void main(String[] args) {

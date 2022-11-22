651c68414dae4a96ce7d62b3d13f94faa410a578
Hyunebin <qortjr83@naver.com>
2022-11-22 23:16:57 +0900

    Initial commit

---
+++ src/main/java/com/buildup/bu/BuApplication.java
@@ -0,0 +1,13 @@
+package com.buildup.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuApplication.class, args);
+    }
+
+}

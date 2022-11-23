cbed45e108c2e24a25452e92837680cf9e0888b5
Hyunebin <qortjr83@naver.com>
2022-11-23 18:07:05 +0900

    FEAT : 실험 Body(Description) : 실험 Footer : 실험

---
+++ src/main/java/com/buildup/bu/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.buildup.bu.Repository;
+
+import com.buildup.bu.Persist.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
